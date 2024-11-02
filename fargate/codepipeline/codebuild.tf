b26be8672a1ca1f75503903a2140e847d592c60c
puybr <56723522+puybr@users.noreply.github.com>
2024-11-02 18:05:14 +0200

    Update codebuild.tf

--- fargate/codepipeline/codebuild.tf
+++ fargate/codepipeline/codebuild.tf
@@ -4,7 +4,7 @@ resource "aws_codebuild_project" "codebuild_project_build_stage" {
   service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
-    type = "S3"
+    type = "CODEPIPELINE"
   }
 
   environment {
