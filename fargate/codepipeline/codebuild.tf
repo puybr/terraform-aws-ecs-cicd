dee095674e0178a541d27977afe3bc94a5ab8c74
puybr <56723522+puybr@users.noreply.github.com>
2024-11-02 17:55:25 +0200

    Update codebuild.tf

--- fargate/codepipeline/codebuild.tf
+++ fargate/codepipeline/codebuild.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "codebuild_project_build_stage" {
-  name          = var.codebuild_project_name
+  name          = var.app_name
   description   = "Build Stage for CodePipeline"
   service_role  = aws_iam_role.codebuild_role.arn
 
