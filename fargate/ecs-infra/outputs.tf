6c18b4ecf77e5f1e07e9247131c41027a0fb1aa4
puybr <56723522+puybr@users.noreply.github.com>
2024-11-02 18:08:34 +0200

    Update outputs.tf

--- fargate/ecs-infra/outputs.tf
+++ fargate/ecs-infra/outputs.tf
@@ -1,23 +1,3 @@
 output "lb_hostname" {
   value = "${aws_alb.main.dns_name}:${var.app_port}"
 }
-
-output "lb_listener_arn" {
-  value = aws_alb_listener.front_end.arn
-}
-
- output "lb_target_group_names" {
-    value = aws_alb_target_group.app[*].name
- }
-
-output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.app.arn
-}
-
-output "aws_ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
-}
-
-output "aws_ecs_service_name" {
-  value = aws_ecs_service.main.name
-}
