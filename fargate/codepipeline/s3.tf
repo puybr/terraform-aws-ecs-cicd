739ec81aacc6c1e879a55d4462f25ab818feca6d
puybr <56723522+puybr@users.noreply.github.com>
2024-11-03 14:20:51 +0200

    Update s3.tf

--- fargate/codepipeline/s3.tf
+++ fargate/codepipeline/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = var.s3_bucket_name
+  bucket = "${var.app_name}-${var.s3_bucket_name}"
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
