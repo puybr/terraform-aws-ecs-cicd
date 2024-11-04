9cae710865ff96183c02dd41732f2c6b415e3521
puybr <56723522+puybr@users.noreply.github.com>
2024-11-04 13:55:06 +0000

    Update ecs-iam.tf

--- fargate/ecs-infra/ecs-iam.tf
+++ fargate/ecs-infra/ecs-iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.ecs_task_execution_role_name}"
   assume_role_policy = <<EOF
-        {
-        "Version": "2012-10-17",
-        "Statement": [
-          {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-              "Service": "ecs-tasks.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-          }
-        ]
-        }
-        EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
 }
 
 resource "aws_iam_role_policy" "ecs_task_execution_policy" {
