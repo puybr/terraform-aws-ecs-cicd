660c0e248de307b71465800b254b8dd111d8e156
Chloe Urisohn <56723522+ghost-goblin@users.noreply.github.com>
2025-06-01 10:17:01 +0100

    refactor modules

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+# Retrieve availability zones for the current region
+data "aws_availability_zones" "available" {}
+
+# Retreive current account ID
+data "aws_caller_identity" "current" {}
+
+# Retreive current region
+data "aws_region" "current" {}
+
+# Locals
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+# Create S3 bucket for codepipeline artifacts
 resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "${var.app_name}-${uuid()}"
 }
@@ -12,5 +27,4 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
   bucket     = aws_s3_bucket.codepipeline_bucket.id
   acl        = "private"
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
