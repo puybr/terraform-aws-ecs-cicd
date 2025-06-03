019b85425280abd8e1537e1eddc3cedc726da84a
puybr <56723522+puybr@users.noreply.github.com>
2025-06-03 09:30:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ data "aws_region" "current" {}
 # Locals
 locals {
   account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
 }
 
 # Create S3 bucket for codepipeline artifacts
